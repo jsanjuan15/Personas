7e57a740f8fde3dcbf6bec2cf8f01c2635f589ab
jesus <jsanjuan15@cuc.edu.co>
2017-09-19 21:02:00 -0500

    terminado

---
+++ app/src/main/java/com/example/android/personas/Datos.java
@@ -0,0 +1,20 @@
+package com.example.android.personas;
+
+import java.util.ArrayList;
+
+/**
+ * Created by android on 19/09/2017.
+ */
+
+public class Datos {
+    private static ArrayList<Persona> personas= new ArrayList<>();
+
+    public static void guardar(Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona>  obtener(){
+        return personas;
+
+    }
+}
